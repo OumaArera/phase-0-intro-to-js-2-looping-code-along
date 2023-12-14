55b7225266c801e471ee4db502e10cbf9f4babfe
OumaArera <johnouma999@gmail.com>
2023-12-14 22:18:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfStringNames, event){
+    let arrayName = [];
+    for (let i = 0; i < arrayOfStringNames.length; i++){
+        let message = `Thank you, ${arrayOfStringNames[i]}, for the wonderful ${event} gift!`;
+        arrayName.push(message);
+
+    }
+    return arrayName;
+}
+
+function countDown(positiveInteger){
+    let i = positiveInteger; 
+    while (i>=0){
+        console.log(i--);
+    }
+
+}
